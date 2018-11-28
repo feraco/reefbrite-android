f42f3114ee804fdad3a52fd79a993c00497421f2
lindsey <lindseyxialin@gmail.com>
2018-11-28 16:10:36 -0500

    add percentage to the slider

---
+++ app/src/main/java/com/nordicsemi/reefBrite/BrightnessActivity.java
@@ -0,0 +1,102 @@
+package com.nordicsemi.reefBrite;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class BrightnessActivity extends Activity {
+    private Button btnBack, btnBackButtom;
+    private SeekBar blue, white;
+    public static Activity brightnessActivity;
+    private TextView percentage1, percentage2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.brightness_controller);
+        MainActivity.activityRunningState=3;
+        brightnessActivity = this;
+
+        btnBack = (Button) findViewById(R.id.go_back_btn_3);
+        blue = (SeekBar) findViewById(R.id.blue_bar);
+        white = (SeekBar) findViewById(R.id.white_bar);
+        btnBackButtom = (Button) findViewById(R.id.btn_back);
+        percentage1 = (TextView) findViewById(R.id.percentage1);
+        percentage2 = (TextView) findViewById(R.id.percentage2);
+
+        blue.setProgress(MainActivity.currBlueValue);
+        percentage1.setText((int)(MainActivity.currBlueValue/2.55)+"%");
+        white.setProgress(MainActivity.currWhiteValue);
+        percentage2.setText((int)(MainActivity.currWhiteValue/2.55)+"%");
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.indexEdit = -1;
+                MainActivity.activityRunningState=0;
+                byte[] value = new byte[]{7};
+                MainActivity.mService.writeRXCharacteristic(value);
+                finish();
+            }
+        });
+
+        btnBackButtom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.indexEdit = -1;
+                MainActivity.activityRunningState=0;
+                byte[] value = new byte[]{7};
+                MainActivity.mService.writeRXCharacteristic(value);
+                finish();
+            }
+        });
+
+
+        blue.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                percentage1.setText((int)(i/2.55)+"%");
+                byte[] value = new byte[2];
+                value[0] = 3;
+                value[1] = (byte)i;
+                MainActivity.mService.writeRXCharacteristic(value);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        white.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                percentage2.setText((int)(i/2.55)+"%");
+                byte[] value = new byte[2];
+                value[0] = 2;
+                value[1] = (byte)i;
+                MainActivity.mService.writeRXCharacteristic(value);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/nordicsemi/reefBrite/PointActivity.java
@@ -0,0 +1,198 @@
+package com.nordicsemi.reefBrite;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Comparator;
+
+public class PointActivity extends Activity {
+
+    private Button btnBack, btnSave;
+    private TimePicker picker;
+    private SeekBar blue, white;
+    public static Activity pointActivity;
+    private TextView percentage1, percentage2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.point_controller);
+        pointActivity = this;
+        MainActivity.activityRunningState=2;
+
+        btnBack = (Button) findViewById(R.id.go_back_btn_2);
+        btnSave = (Button) findViewById(R.id.btn_save);
+        picker=(TimePicker)findViewById(R.id.timePicker);
+        blue = (SeekBar) findViewById(R.id.blue_bar);
+        white = (SeekBar) findViewById(R.id.white_bar);
+        percentage1 = (TextView) findViewById(R.id.percentage1);
+        percentage2 = (TextView) findViewById(R.id.percentage2);
+
+        if(MainActivity.indexEdit != -1){
+            PointModel pModel = MainActivity.pApter.getItem(MainActivity.indexEdit);
+            picker.setCurrentHour(pModel.getHour());
+            picker.setCurrentMinute(pModel.getMinu());
+            blue.setProgress(pModel.getBlueV());
+            percentage1.setText((int)(pModel.getBlueV()/2.55)+"%");
+            white.setProgress(pModel.getWhiteV());
+            percentage2.setText((int)(pModel.getWhiteV()/2.55)+"%");
+        }else{
+            blue.setProgress(MainActivity.currBlueValue);
+            percentage1.setText((int)(MainActivity.currBlueValue/2.55)+"%");
+            white.setProgress(MainActivity.currWhiteValue);
+            percentage2.setText((int)(MainActivity.currWhiteValue/2.55)+"%");
+        }
+
+        byte[] value = new byte[2];
+        value[0] = 3;
+        value[1] = (byte)blue.getProgress();
+        MainActivity.mService.writeRXCharacteristic(value);
+        value[0] = 2;
+        value[1] = (byte)white.getProgress();
+        MainActivity.mService.writeRXCharacteristic(value);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.indexEdit = -1;
+                MainActivity.activityRunningState=0;
+                byte[] value = new byte[]{7};
+                MainActivity.mService.writeRXCharacteristic(value);
+                finish();
+            }
+        });
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isDuplicate = false;
+
+                for(int i=0; i<MainActivity.pApter.getCount(); i++ ){
+                    if(MainActivity.pApter.getItem(i).getHour()==picker.getCurrentHour() && MainActivity.pApter.getItem(i).getMinu()==picker.getCurrentMinute()){
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+
+                if(MainActivity.indexEdit != -1){
+                    PointModel pModel = MainActivity.pApter.getItem(MainActivity.indexEdit);
+                    if(!isDuplicate){
+                        pModel.setHour(picker.getCurrentHour());
+                        pModel.setMinu(picker.getCurrentMinute());
+                        pModel.setBlueV(blue.getProgress());
+                        pModel.setWhiteV(white.getProgress());
+                        sortAdpter(MainActivity.pApter);
+                        MainActivity.pApter.notifyDataSetChanged();
+                        MainActivity.indexEdit = -1;
+                        MainActivity.activityRunningState=0;
+                        finish();
+                    }else{
+                        if(pModel.getHour()==picker.getCurrentHour() && pModel.getMinu()==picker.getCurrentMinute()) {
+                            pModel.setBlueV(blue.getProgress());
+                            pModel.setWhiteV(white.getProgress());
+                            MainActivity.pApter.notifyDataSetChanged();
+                            MainActivity.indexEdit = -1;
+                            MainActivity.activityRunningState=0;
+                            finish();
+                        }else{
+                            AlertDialog alertDialog = new AlertDialog.Builder(PointActivity.this).create();
+                            alertDialog.setTitle("ERROR!");
+                            alertDialog.setMessage("The time already set in the schedule.");
+                            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Close",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            dialog.dismiss();
+                                        }
+                                    });
+                            alertDialog.show();
+                        }
+                    }
+
+                }else{
+                    if(!isDuplicate){
+                        MainActivity.pApter.add(new PointModel(picker.getCurrentHour(), picker.getCurrentMinute(), blue.getProgress(), white.getProgress()));
+                        MainActivity.indexEdit = -1;
+                        sortAdpter(MainActivity.pApter);
+                        MainActivity.pApter.notifyDataSetChanged();
+                        MainActivity.activityRunningState=0;
+                        finish();
+                    }else{
+                        AlertDialog alertDialog = new AlertDialog.Builder(PointActivity.this).create();
+                        alertDialog.setTitle("ERROR!");
+                        alertDialog.setMessage("The time already set in the schedule.");
+                        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Close",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                });
+                        alertDialog.show();
+                    }
+                }
+
+                MainActivity.sendEvent();
+            }
+        });
+
+        blue.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                percentage1.setText((int)(i/2.55)+"%");
+                byte[] value = new byte[2];
+                value[0] = 3;
+                value[1] = (byte)i;
+                MainActivity.mService.writeRXCharacteristic(value);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        white.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                percentage2.setText((int)(i/2.55)+"%");
+                byte[] value = new byte[2];
+                value[0] = 2;
+                value[1] = (byte)i;
+                MainActivity.mService.writeRXCharacteristic(value);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+    public void sortAdpter(PointAdapter pApter){
+        pApter.sort(new Comparator<PointModel>() {
+            @Override
+            public int compare(PointModel p1, PointModel p2) {
+                return p1.getTimeInMinu()-p2.getTimeInMinu();   //or whatever your sorting algorithm
+            }
+        });
+    }
+}
